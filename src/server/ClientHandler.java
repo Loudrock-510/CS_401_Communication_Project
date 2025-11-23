72c0b63888d93c64d657195d238c5787a427b50b
BaoDoii <BRIANHA29@GMAIL.COM>
2025-11-22 17:19:21 -0800

    Implementing ClientHander

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,5 +1,46 @@
 package server;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.*; //package for multithreading (ExecutorService, ThreadPool, ConcurrentHashMap)
 
+import server.User;
+
+public class ClientHandler implements Runnable{
+	
+	private Socket socket;
+	private Server server;
+	//threadpool
+	private ExecutorService threadPool = Executors.newCachedThreadPool();
+
+	//map
+	private Map<String, Socket> clientSockets = new ConcurrentHashMap<>();
+	private Map<String, ObjectInputStream> clientInput = new ConcurrentHashMap<>();
+	private Map<String, ObjectOutputStream> clientOut = new ConcurrentHashMap<>();
+	
+	private User concurrentUser; //user attached to this handler
+	
+	//constructor
+	public ClientHandler(Socket socket, Server server) {
+		this.socket = socket;
+		this.server = server;
+	}
+	
+	@Override
+	public void run() {
+		startManager();
+	}
+	
+	public void startManager(){
+		try{
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			
+			private User loggedInUser = null; //after login becomes non null
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
