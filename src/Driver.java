00ae3ef5476703b6ccee86fd0e97d655c600c3e0
Matthew Ramirez <mramirez189@horizon.csueastbay.edu>
2025-12-02 13:10:51 -0800

    Improved driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import server.*;
+
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println("Hello world test!");
+		Server server = new Server(59090);
 	}
 }
